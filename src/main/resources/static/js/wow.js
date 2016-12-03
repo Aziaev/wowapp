7aec17fe449498aaf8bcfb6fa948676bc0b46a8d
Aziaev <aziaev@gmail.com>
2016-12-04 01:02:52 +0300

    big release. Pretty close to final

--- src/main/resources/static/js/wow.js
+++ src/main/resources/static/js/wow.js
@@ -12,3 +12,8 @@ $('.carousel.carousel-slider').carousel({full_width: true});
 $(document).ready(function(){
     $('.tooltipped').tooltip({delay: 50});
 });
+
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+});
